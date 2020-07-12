64e74573fae7e4a954bdaeb6672668dc882ad036
d-serj <serj.dub1na@gmail.com>
2020-07-12 06:27:35 -0700

    Add device-vendor.mk file
    File added to build Led control packages and HAL within Android build system
    
    Signed-off-by: d-serj <serj.dub1na@gmail.com>

---
+++ device-vendor.mk
@@ -0,0 +1,15 @@
+# Makefile for vendor components
+
+PRODUCT_PACKAGES += \
+	vendor.gl.hardware.ledcontrol@1.0-service \
+	LedControlService \
+	LedControlApp
+
+TARGET_FS_CONFIG_GEN += \
+	vendor/gl/hardware/interfaces/config.fs
+
+DEVICE_MANIFEST_FILE += \
+	vendor/gl/hardware/interfaces/manifest.xml
+
+DEVICE_MATRIX_FILE += \
+	vendor/gl/hardware/interfaces/compatibility_matrix.xml
