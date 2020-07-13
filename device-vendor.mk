d9cbf679a36310ce058adb41f51555365bfff706
d-serj <serj.dub1na@gmail.com>
2020-07-13 13:28:07 -0700

    Add SELinux policies to device-vendor.mk
    Signed-off-by: d-serj <serj.dub1na@gmail.com>

--- device-vendor.mk
+++ device-vendor.mk
@@ -5,11 +5,12 @@ PRODUCT_PACKAGES += \
 	LedControlService \
 	LedControlApp
 
-TARGET_FS_CONFIG_GEN += \
-	vendor/gl/hardware/interfaces/config.fs
+TARGET_FS_CONFIG_GEN += vendor/gl/hardware/interfaces/config.fs
 
 DEVICE_MANIFEST_FILE += \
 	vendor/gl/hardware/interfaces/manifest.xml
 
 DEVICE_MATRIX_FILE += \
 	vendor/gl/hardware/interfaces/compatibility_matrix.xml
+
+BOARD_SEPOLICY_DIRS += vendor/gl/sepolicy
